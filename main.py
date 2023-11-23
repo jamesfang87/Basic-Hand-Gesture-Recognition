c3435067dbe0357f4113fcc4c19ac3560ff16736
James Fang <113867753+jamesfang87@users.noreply.github.com>
2023-11-22 19:17:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from util import *
+from classification import classify, examples
 
-video_path = "three.mov"
+video_path = ""
+data = detect("out", 0)
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
 
-cap = cv2.VideoCapture(video_path)
-data = detect("out", 1, "three.mov")
-
-cap.release()
-cv2.destroyAllWindows()
 print(data)
+
+classify([data], examples)
