c0c77f4f531ad4ddf92901a97709eb87ccef602d
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-01-05 23:01:58 -0600

    Update util.py

--- util.py
+++ util.py
@@ -3,19 +3,16 @@ import mediapipe as mp
 import numpy as np
 
 
-def write(file_path, landmarks):
-    f = open(file_path, "a")
+def extract_ptr_finger_location(landmarks):
     output = str(landmarks).split("landmark {")[9].split("}")[0].split('\n  ')[1:]
     output[-1] = output[-1].strip('\n')
     for i in range(len(output)):
         output[i] = float(output[i][3:])
-    f.write(str(output[0]) + ',' + str(output[1]) + '\n')
     return output[0:2]
 
 
-def detect(output_path, option, video_path=None):
+def detect(option, video_path=None):
     """
-    :param output_path: path to the file to write the detection results
     :param option: value of 0 or 1 where 0 represents livestream and 1 represents video upload
     :param video_path: path to the video if option is video upload.  defaulted to None
     :return:
@@ -42,13 +39,13 @@ def detect(output_path, option, video_path=None):
 
         if results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, results.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
-            data.append(write(output_path, results.multi_hand_landmarks[0]))
+            data.append(extract_ptr_finger_location(results.multi_hand_landmarks[0]))
 
         cv2.imshow('MediaPipe Pose', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
     cap.release()
     cv2.destroyAllWindows()
+
     return np.array(data)
